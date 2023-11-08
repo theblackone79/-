048ba7076a24efa927ecc08b782bda2980dda6fc
theblackone79 <150225131+theblackone79@users.noreply.github.com>
2023-11-08 12:02:11 +0300

    Add files via upload

---
+++ практика грачёва 2.cpp
@@ -0,0 +1,23 @@
+﻿// ConsoleApplication4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    int n, сумма;
+
+    
+    std::cout << "Введите значение n: ";
+    std::cin >> n;
+
+    
+    сумма = n * (n + 1) / 2;
+
+   
+    std::cout << "Сумма чисел от 1 до " << n << " равна " << сумма << std::endl;
+
+    return 0;
+}
+
